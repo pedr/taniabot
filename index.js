858b3aed65ccc4bfc8ead8b6b8f10d726371c8b4
pedro l <pedrlz.frn@gmail.com>
2019-05-26 17:22:47 -0300

    asdf

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const port = process.env.PORT || 5000;
+const token = process.env.TOKEN;
+const port = process.env.PORT;
 const host = process.env.HOST;
+const externalUrl = process.env.EXTERNAL_URL;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, webHook : { port });
+const bot = new TelegramBot(token, { webHook: { port, host }, polling : true });
+bot.setWebHook(externalUrl + ':443/bot' + token);
 const PLAYLISTS = {};
 
 bot.on('message', (msg) => {
