256e1cc73ca09eb6641a591092949d2efbfefee9
pedro l <pedrlz.frn@gmail.com>
2021-01-30 12:36:44 -0300

    colocando bot atras de um servidor web

--- configs.js
+++ configs.js
@@ -1,19 +1,36 @@
 require('dotenv').config()
 
+const TelegramBot = require('node-telegram-bot-api')
+const express = require('express');
+
+
 const token = process.env.TOKEN;
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || '0.0.0.0';
-const externalUrl = process.env.EXTERNAL_URL;
+const externalUrl = process.env.EXTERNAL_URL || "https://60dd4f94cd7c.ngrok.io";
 
-const TelegramBot = require('node-telegram-bot-api')
-let bot
-
-if (process.env.STAGE == 'dev') {
-    bot = new TelegramBot(token, { polling: true })
-} else {
-    bot = new TelegramBot(token, { webHook: { port : port, host : host } });
-    
-    bot.setWebHook(externalUrl + ':443/bot' + token);
+if (process.env.stage != "DEV") {
+    throw "ARRUMA O EXTERNALURL"
 }
+
+const app = express();
+app.use(express.json());
+
+let bot = new TelegramBot(token)
+bot.setWebHook(`${externalUrl}:443/bot${token}`);
+
+app.post(`/bot${token}`, (req, res) => {
+    console.log('alo algo aconteceu')
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+})
+
+app.get("/", (req, res) => {
+    res.send("<p>hello world</p>");
+})
+
+app.listen(port, () => {
+    console.log(`server rodando na porta ${port}`);
+})
 
 module.exports = bot;
